dd7ff728bb9dfa0be57a5e2176114e9d42181a9a
Jason Garman <jgarman@bit9.com>
2016-02-01 12:41:26 -0500

    Update changelog and document S3 improvements

--- net_output.go
+++ net_output.go
@@ -4,8 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
@@ -16,14 +20,20 @@ type NetOutput struct {
 	outputSocket   net.Conn
 	addNewline     bool
 
-	connectTime time.Time
+	connectTime       time.Time
+	reconnectTime     time.Time
+	connected         bool
+	droppedEventCount int64
+
 	sync.RWMutex
 }
 
 type NetStatistics struct {
-	LastOpenTime   time.Time `json:"last_open_time"`
-	Protocol       string    `json:"connection_protocol"`
-	RemoteHostname string    `json:"remote_hostname"`
+	LastOpenTime      time.Time `json:"last_open_time"`
+	Protocol          string    `json:"connection_protocol"`
+	RemoteHostname    string    `json:"remote_hostname"`
+	DroppedEventCount int64     `json:"dropped_event_count"`
+	Connected         bool      `json:"connected"`
 }
 
 // Initialize() expects a connection string in the following format:
@@ -32,6 +42,10 @@ type NetStatistics struct {
 func (o *NetOutput) Initialize(netConn string) error {
 	o.Lock()
 	defer o.Unlock()
+
+	if o.connected {
+		o.outputSocket.Close()
+	}
 
 	o.netConn = netConn
 
@@ -52,8 +66,25 @@ func (o *NetOutput) Initialize(netConn string) error {
 	}
 
 	o.connectTime = time.Now()
+	o.connected = true
+
+	// we need a way to ensure that we don't block on the output. We will disconnect and reconnect if this timeout
+	// occurs
+	o.outputSocket.SetWriteDeadline(time.Duration(500 * time.Millisecond))
 
 	return nil
+}
+
+func (o *NetOutput) closeAndScheduleReconnection() {
+	o.Lock()
+	defer o.Unlock()
+
+	if o.connected {
+		o.outputSocket.Close()
+	}
+
+	// try reconnecting in 30 seconds
+	o.reconnectTime = time.Now() + time.Duration(30*time.Second)
 }
 
 func (o *NetOutput) Key() string {
@@ -74,12 +105,24 @@ func (o *NetOutput) Statistics() interface{} {
 	o.RLock()
 	defer o.RUnlock()
 
-	return NetStatistics{LastOpenTime: o.connectTime, Protocol: o.protocolName, RemoteHostname: o.remoteHostname}
+	return NetStatistics{
+		LastOpenTime:      o.connectTime,
+		Protocol:          o.protocolName,
+		RemoteHostname:    o.remoteHostname,
+		DroppedEventCount: o.droppedEventCount,
+		Connected:         o.connected,
+	}
 }
 
 func (o *NetOutput) output(m string) error {
 	if o.addNewline {
 		m = m + "\r\n"
+	}
+
+	if !o.connected {
+		// drop this event on the floor...
+		atomic.AddInt64(o.droppedEventCount, 1)
+		return nil
 	}
 
 	_, err := o.outputSocket.Write([]byte(m))
@@ -90,6 +133,10 @@ func (o *NetOutput) output(m string) error {
 			return err
 		}
 		_, err = o.outputSocket.Write([]byte(m))
+		// if we still have an error...
+		if err != nil {
+			o.closeAndScheduleReconnection()
+		}
 		return err
 	}
 	return err
@@ -101,12 +148,28 @@ func (o *NetOutput) Go(messages <-chan string, errorChan chan<- error) error {
 	}
 
 	go func() {
+		refreshTicker := time.NewTicker(1 * time.Second)
+		defer refreshTicker.Stop()
+
+		hup := make(chan os.Signal, 1)
+		signal.Notify(hup, syscall.SIGHUP)
+
+		defer signal.Stop(hup)
+
 		for {
 			select {
 			case message := <-messages:
 				if err := o.output(message); err != nil {
 					errorChan <- err
 					return
+				}
+
+			case <-refreshTicker.C:
+				if !o.connected && time.Now() > o.reconnectTime {
+					err := o.Initialize(o.netConn)
+					if err != nil {
+						o.closeAndScheduleReconnection()
+					}
 				}
 			}
 		}
