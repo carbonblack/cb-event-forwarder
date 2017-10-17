524341584c8886c7c768397ca50f57b0b2de792d
Zachary Estep <zestep@carbonblack.com>
2017-10-17 09:26:52 -0400

    Moving logic down a bit

--- json_message_processor.go
+++ json_message_processor.go
@@ -137,14 +137,6 @@ func fixupMessage(messageType string, msg map[string]interface{}) {
 	if !strings.HasPrefix(messageType, "alert.") {
 		if value, ok := msg["unique_id"]; ok {
 			if uniqueId, ok := value.(string); ok {
-                if (strings.HasPrefix(messageType,"feed.storage.hit")){
-			      if segment, ok := msg["segment_id"] ; ok {
-	                if seg , ok := segment.(string); ok {
-	                    uniqueId += "-" + seg
-	                    log.Printf("Added segment to uniqueID = %s\n",uniqueId)
-	                }
-	              }
-	            }
 				processGuid, segment, err := parseFullGuid(uniqueId)
 				if err == nil {
 					msg["process_guid"] = processGuid
@@ -159,6 +151,14 @@ func fixupMessage(messageType string, msg map[string]interface{}) {
 	if !hasProcessGUID {
 		if value, ok := msg["process_id"]; ok {
 			if uniqueId, ok := value.(string); ok {
+			    if (strings.HasPrefix(messageType,"feed.storage.hit")){
+			      if segment, ok := msg["segment_id"] ; ok {
+	                if seg , ok := segment.(string); ok {
+	                    uniqueId += "-" + seg
+	                    log.Printf("Added segment to uniqueID = %s\n",uniqueId)
+	                }
+	              }
+	            }
 				processGuid, segment, _ := parseFullGuid(uniqueId)
 				msg["process_guid"] = processGuid
 				msg["segment_id"] = fmt.Sprintf("%v", segment)
