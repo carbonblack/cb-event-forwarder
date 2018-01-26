23da2e676dd9cf234084a538f9f4e717ecf77fe9
Jason McFarland <jmcfarland@carbonblack.com>
2018-01-26 11:10:00 -0600

    fixing internal imports

--- cmd/cb-event-forwarder/pb_message_processor.go
+++ cmd/cb-event-forwarder/pb_message_processor.go
@@ -6,7 +6,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/carbonblack/pkg/sensor_events"
+	"github.com/carbonblack/cb-event-forwarder/internal/sensor_events"
 	"github.com/golang/protobuf/proto"
 	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
