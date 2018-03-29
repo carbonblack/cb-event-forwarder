b966c2390ccb6027de6c79106ee01060c55d3d46
Zachary Estep <zestep@carbonblack.com>
2018-03-29 14:49:55 -0400

    Changing to kafka-python instead of confluent-kafka to reduce installation pains

--- cmd/cb-event-forwarder/kafka_util.py
+++ cmd/cb-event-forwarder/kafka_util.py
@@ -1,23 +1,23 @@
-#pip install confluent-kafka
+#pip install kafka-python
 import sys
 import glob
 import json
-from confluent_kafka import Producer
+from kafka import KafkaProducer
 
 def main(args):
     arg_len = len(args)
     if arg_len < 2:
         print ("usage: kafka_util.py broker1:port1,broker2,port2 /path/to/json/files/like_*.json optional_topic_suffix")
         exit()
-    brokers = args[0].split(",")
+    brokers = args[0]
     path = args[1]
     files = glob.glob(path)
     topic_suffix = "" if len(args) < 3 else args[2]
-    p = Producer({'bootstrap.servers': brokers})
+    p = KafkaProducer(bootstrap_servers=brokers,value_serializer=lambda s: s.encode('utf-8'))
     for f in files: 
         for data in open(f,'r').readlines():
                 data_dict = json.loads(data)
-                p.produce(data_dict['type'].replace('ingress.event','')+topic_suffix, data.encode('utf-8'))
+                p.send(data_dict['type'].replace('ingress.event','')+topic_suffix, data)
                 p.flush()
         
 if __name__ == "__main__":
