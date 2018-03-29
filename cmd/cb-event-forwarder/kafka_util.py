a2809bf6e460e1ab366afb11dfaa8f2a4ebcccd9
Zachary Estep <zestep@carbonblack.com>
2018-03-29 14:30:20 -0400

    updates to util

--- cmd/cb-event-forwarder/kafka_util.py
+++ cmd/cb-event-forwarder/kafka_util.py
@@ -4,15 +4,18 @@ import glob
 from confluent_kafka import Producer
 
 def main(args):
+    arg_len = len(args)
+    if arg_len < 2:
+        print ("usage: kafka_util.py broker1:port1,broker2,port2 /path/to/json/files/like_*.json optional_topic_suffix")
     brokers = args[0].split(",")
     path = args[1]
     files = glob.glob(path)
     topic_suffix = "" if len(args) < 3 else args[2]
-    p = Producer({'bootstrap.servers': brokers)})
+    p = Producer({'bootstrap.servers': brokers})
     for f in files: 
         for data in open(f,'r').readlines():
                 data_dict = json.loads(data)
-                p.produce(topic_suffix+data_dict['type'].replace('ingress.event',''), data.encode('utf-8'))
+                p.produce(data_dict['type'].replace('ingress.event','')+topic_suffix, data.encode('utf-8'))
                 p.flush()
         
 if __name__ == "__main__":
