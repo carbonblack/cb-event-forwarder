7185cb92ea07cfd4e4ae34d9485aa703f9cb3976
Jason Garman <jgarman@carbonblack.com>
2018-04-04 09:45:38 -0400

    add fork_pid to event messages if it exists

--- cmd/cb-event-forwarder/pb_message_processor.go
+++ cmd/cb-event-forwarder/pb_message_processor.go
@@ -345,6 +345,10 @@ func ProcessProtobufMessage(routingKey string, body []byte, headers amqp.Table) 
 		outmsg["process_guid"] = processGUID
 		outmsg["pid"] = inmsg.OriginalMessage.Header.GetProcessPid()
 
+		if inmsg.OriginalMessage.Header.ForkPid != nil {
+			outmsg["fork_pid"] = inmsg.OriginalMessage.Header.GetForkPid()
+		}
+
 		/*
 		 * Sometimes Process path is empty
 		 */
