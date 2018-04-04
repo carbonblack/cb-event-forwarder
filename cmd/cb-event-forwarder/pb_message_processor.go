7e96446722d7dad19e15ae483819cf9a3f4f8949
Jason Garman <jgarman@carbonblack.com>
2018-04-04 09:53:00 -0400

    Change Windows FILETIME to Unix timestamp conversion to use floating point
    This change will add ns precision to the timestamp when it's available in the source data

--- cmd/cb-event-forwarder/pb_message_processor.go
+++ cmd/cb-event-forwarder/pb_message_processor.go
@@ -234,7 +234,7 @@ func ProcessProtobufMessage(routingKey string, body []byte, headers amqp.Table) 
 	}
 
 	outmsg := make(map[string]interface{})
-	outmsg["timestamp"] = WindowsTimeToUnixTime(inmsg.OriginalMessage.Header.GetTimestamp())
+	outmsg["timestamp"] = WindowsTimeToUnixTimeFloat(inmsg.OriginalMessage.Header.GetTimestamp())
 	outmsg["type"] = routingKey
 
 	outmsg["sensor_id"] = cbMessage.Env.Endpoint.GetSensorId()
@@ -405,7 +405,7 @@ func WriteProcessMessage(message *ConvertedCbMessage, kv map[string]interface{})
 	kv["parent_path"] = om.Process.GetParentPath()
 	kv["parent_pid"] = om.Process.GetParentPid()
 	kv["parent_guid"] = om.Process.GetParentGuid()
-	kv["parent_create_time"] = WindowsTimeToUnixTime(om.Process.GetParentCreateTime())
+	kv["parent_create_time"] = WindowsTimeToUnixTimeFloat(om.Process.GetParentCreateTime())
 	kv["filtering_known_dlls"] = om.Process.GetBFilteringKnownDlls()
 
 	if message.OriginalMessage.Process.ParentMd5 != nil {
