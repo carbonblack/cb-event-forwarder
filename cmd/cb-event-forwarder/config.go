57eb8dd522a2b724d2cc35cf3171e7d6e9a8aa53
Mats Christensen <mats.christensen@sparebank1.no>
2018-12-12 16:51:22 +0100

    Fixed identations

--- cmd/cb-event-forwarder/config.go
+++ cmd/cb-event-forwarder/config.go
@@ -96,10 +96,10 @@ type Configuration struct {
 	KafkaBrokers        *string
 	KafkaTopicSuffix    string
 	KafkaTopic			string
-    KafkaProtocol       string
-    KafkaMechanism      string
-    KafkaUsername       string
-    KafkaPassword       string
+	KafkaProtocol       string
+	KafkaMechanism      string
+	KafkaUsername       string
+	KafkaPassword       string
 	KafkaMaxRequestSize int32
 
 	//Splunkd
@@ -505,26 +505,26 @@ func ParseConfig(fn string) (Configuration, error) {
 			if ok {
 				config.KafkaTopicSuffix = kafkaTopicSuffix
 			}
-            kafkaTopic, ok := input.Get("kafka", "topic")
-            if ok {
-                config.KafkaTopic = kafkaTopic
-            }
-            kafkaProtocol, ok := input.Get("kafka", "protocol")
-            if ok {
-                config.KafkaProtocol = kafkaProtocol
-            }
-            kafkaMechanism, ok := input.Get("kafka", "mechanism")
-            if ok {
-                config.KafkaMechanism = kafkaMechanism
-            }
-            kafkaUsername, ok := input.Get("kafka", "username")
-            if ok {
-                config.KafkaUsername = kafkaUsername
-            }
-            kafkaPassword, ok := input.Get("kafka", "password")
-            if ok {
-                config.KafkaPassword = kafkaPassword
-            }
+			kafkaTopic, ok := input.Get("kafka", "topic")
+			if ok {
+				config.KafkaTopic = kafkaTopic
+			}
+			kafkaProtocol, ok := input.Get("kafka", "protocol")
+			if ok {
+				config.KafkaProtocol = kafkaProtocol
+			}
+			kafkaMechanism, ok := input.Get("kafka", "mechanism")
+			if ok {
+				config.KafkaMechanism = kafkaMechanism
+			}
+			kafkaUsername, ok := input.Get("kafka", "username")
+			if ok {
+				config.KafkaUsername = kafkaUsername
+			}
+			kafkaPassword, ok := input.Get("kafka", "password")
+			if ok {
+				config.KafkaPassword = kafkaPassword
+			}
 			kafkaMaxRequestSize, ok := input.Get("kafka", "max_request_size")
 			if ok {
 				if intKafkaMaxRequestSize, err := strconv.ParseInt(kafkaMaxRequestSize, 10, 32); err == nil {
