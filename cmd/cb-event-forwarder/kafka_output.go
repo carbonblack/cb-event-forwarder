dab57137bb4d2a38d6f4b85585be6c053d318013
Zachary Estep <zestep@carbonblack.com>
2020-06-04 16:17:32 -0400

    CB-31401 - Use sarama instead of confluent golang client

--- cmd/cb-event-forwarder/kafka_output.go
+++ cmd/cb-event-forwarder/kafka_output.go
@@ -3,50 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/rcrowley/go-metrics"
-	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"syscall"
+	"time"
+
+	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
 )
-
-// Producer implements a High-level Apache Kafka Producer instance ZE 2018
-// This allows Mocking producers w/o actual contact to kafka broker for testing purposes
-type WrappedProducer interface {
-	String() string
-
-	Produce(msg *kafka.Message, deliveryChan chan kafka.Event) error
-
-	Events() chan kafka.Event
-
-	ProduceChannel() chan *kafka.Message
-
-	Len() int
-
-	Flush(timeoutMs int) int
-
-	Close()
-
-	GetMetadata(topic *string, allTopics bool, timeoutMs int) (*kafka.Metadata, error)
-
-	QueryWatermarkOffsets(topic string, partition int32, timeoutMs int) (low, high int64, err error)
-
-	OffsetsForTimes(times []kafka.TopicPartition, timeoutMs int) (offsets []kafka.TopicPartition, err error)
-}
 
 type KafkaOutput struct {
 	brokers           []string
 	topicSuffix       string
-	topic             string
-	producers         []WrappedProducer
+	producer          sarama.AsyncProducer
 	droppedEventCount int64
 	eventSentCount    int64
-	EventSent         metrics.Meter
-	EventSentBytes    metrics.Meter
-	DroppedEvent      metrics.Meter
+
 	sync.RWMutex
 }
 
@@ -59,155 +34,74 @@ func (o *KafkaOutput) Initialize(unused string) error {
 	o.Lock()
 	defer o.Unlock()
 
-	o.brokers = strings.Split(*(config.KafkaBrokers), ",")
+	o.brokers = strings.Split(*config.KafkaBrokers, ",")
 	o.topicSuffix = config.KafkaTopicSuffix
-	o.topic = config.KafkaTopic
-	o.producers = make([]WrappedProducer, len(o.brokers))
 
-	o.EventSent = metrics.NewRegisteredMeter("output.kafka.events_sent", metrics.DefaultRegistry)
-	o.DroppedEvent = metrics.NewRegisteredMeter("output.kafka.events_dropped", metrics.DefaultRegistry)
-	o.EventSentBytes = metrics.NewRegisteredMeter("output.kafka.data_sent", metrics.DefaultRegistry)
+	kafkaConfig := sarama.NewConfig()
+	sarama.MaxRequestSize = config.KafkaMaxRequestSize
+	kafkaConfig.Producer.Return.Successes = true
 
-	var kafkaConfig kafka.ConfigMap = nil
-	//PLAINTEXT, SSL, SASL_PLAINTEXT, SASL_SSL
-	if config.KafkaProducerProps == nil {
-		switch config.KafkaProtocol {
-		case "PLAINTEXT":
-			kafkaConfig = kafka.ConfigMap{"bootstrap.servers": *config.KafkaBrokers}
-		case "SASL":
-			kafkaConfig = kafka.ConfigMap{"bootstrap.servers": *config.KafkaBrokers,
-				"security.protocol": config.KafkaProtocol,
-				"sasl.mechanism":    config.KafkaMechanism,
-				"sasl.username":     config.KafkaUsername,
-				"sasl.password":     config.KafkaPassword}
-		case "SSL":
-			kafkaConfig = kafka.ConfigMap{"bootstrap.servers": *config.KafkaBrokers,
-				"security.protocol": config.KafkaProtocol}
+	producer, err := sarama.NewAsyncProducer(o.brokers, kafkaConfig)
 
-			if config.KafkaSSLCertificateLocation != nil {
-				kafkaConfig["ssl_certificate_location"] = config.KafkaSSLCertificateLocation
-			}
-
-			if config.KafkaSSLKeyLocation != nil && config.KafkaSSLKeyPassword != nil {
-				kafkaConfig["ssl_key_location"] = config.KafkaSSLKeyLocation
-				kafkaConfig["ssl_Key_password"] = config.KafkaSSLKeyPassword
-			}
-
-			if config.KafkaSSLEnabledProtocols != nil {
-				kafkaConfig["ssl.enabled.protocols"] = config.KafkaSSLEnabledProtocols
-			}
-
-			if config.KafkaSSLCALocation != nil {
-				kafkaConfig["ssl.ca.location"] = config.KafkaSSLCALocation
-			}
-		default:
-			kafkaConfig = kafka.ConfigMap{"bootstrap.servers": *config.KafkaBrokers}
-		}
-
-		if config.KafkaCompressionType != nil {
-			kafkaConfig["compression.type"] = *config.KafkaCompressionType
-		}
-	} else {
-		kafkaConfig = config.KafkaProducerProps
+	if err != nil {
+		panic(err)
 	}
 
-	for index, _ := range o.brokers {
-		if config.DryRun {
-			p, err := NewMockedKafkaProducer(".")
-			if err != nil {
-				panic(err)
-			}
-			o.producers[index] = p
-		} else {
-			p, err := kafka.NewProducer(&kafkaConfig)
-			if err != nil {
-				panic(err)
-			}
-			o.producers[index] = p
-		}
-	}
+	o.producer = producer
 
 	return nil
 }
 
 func (o *KafkaOutput) Go(messages <-chan string, errorChan chan<- error) error {
-
-	joinEventsChan := make(chan (kafka.Event), 100000)
-	sigs := make(chan os.Signal, 1)
-	stopProdChans := make([]chan struct{}, len(o.producers))
-
-	signal.Notify(sigs, syscall.SIGHUP)
-	signal.Notify(sigs, syscall.SIGTERM)
-	signal.Notify(sigs, syscall.SIGINT)
-
-	defer signal.Stop(sigs)
-
-	for workernum, producer := range o.producers {
-		stopProdChans[workernum] = make(chan struct{}, 1)
-		go func(workernum int32, producer WrappedProducer, stopProdChan <-chan struct{}) {
-
-			defer producer.Close()
-
-			partition := kafka.PartitionAny
-			shouldStop := false
-			if (len(o.producers)) > 0 {
-				partition = workernum
-			}
-
-			for {
-				select {
-				case message := <-messages:
-					var topic string = config.KafkaTopic
-					if topic == "" {
-						var parsedMsg map[string]interface{}
-						json.Unmarshal([]byte(message), &parsedMsg)
-						topicRaw := parsedMsg["type"]
-						if topicString, ok := topicRaw.(string); ok {
-							topicString = strings.Replace(topicString, "ingress.event.", "", -1)
-							topicString += o.topicSuffix
-							topic = topicString
-						} else {
-							log.Info("ERROR: Topic was not a string")
-						}
-					}
-					partition := kafka.TopicPartition{Topic: &topic, Partition: partition}
-					output(message, o.producers[workernum], partition)
-					o.EventSent.Mark(1)
-					o.EventSentBytes.Mark(int64(len(message)))
-				case <-stopProdChan:
-					shouldStop = true
-				case e := <-producer.Events():
-					joinEventsChan <- e
-				default:
-					if shouldStop {
-						return
-					}
-				}
-			}
-		}(int32(workernum), producer, stopProdChans[workernum])
-	}
 	go func() {
+		refreshTicker := time.NewTicker(1 * time.Second)
+		defer refreshTicker.Stop()
+
+		defer func() {
+			if err := o.producer.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+
+		hup := make(chan os.Signal, 1)
+		signal.Notify(hup, syscall.SIGHUP)
+
+		defer signal.Stop(hup)
+
 		for {
 			select {
-			case e := <-joinEventsChan:
-				m := e.(*kafka.Message)
-				if m.TopicPartition.Error != nil {
-					o.DroppedEvent.Mark(1)
-					errorChan <- m.TopicPartition.Error
-				}
-			case sig := <-sigs:
-				switch sig {
-				case syscall.SIGTERM, syscall.SIGINT:
-					for _, stopChan := range stopProdChans {
-						stopChan <- struct{}{}
-					}
-					return
-				default:
-					log.Debugf("Signal was %s", sig)
+			case message := <-messages:
+				var parsedMsg map[string]interface{}
+
+				json.Unmarshal([]byte(message), &parsedMsg)
+				topic := parsedMsg["type"]
+				if topicString, ok := topic.(string); ok {
+					topicString = strings.Replace(topicString, "ingress.event.", "", -1)
+					topicString += o.topicSuffix
+
+					o.output(topicString, message)
+				} else {
+					log.Info("ERROR: Topic was not a string")
 				}
 			}
 		}
+
 	}()
+
+	go func() {
+		for range o.producer.Successes() {
+			atomic.AddInt64(&o.eventSentCount, 1)
+		}
+	}()
+
+	go func() {
+		for err := range o.producer.Errors() {
+			log.Info(err)
+			atomic.AddInt64(&o.droppedEventCount, 1)
+			errorChan <- err
+		}
+	}()
+
 	return nil
 }
 
@@ -232,17 +126,10 @@ func (o *KafkaOutput) Key() string {
 	return fmt.Sprintf("brokers:%s", o.brokers)
 }
 
-func output(m string, producer WrappedProducer, partition kafka.TopicPartition) {
-	kafkamsg := &kafka.Message{
-		TopicPartition: partition,
-		Value:          []byte(m),
+func (o *KafkaOutput) output(topic string, m string) {
+	o.producer.Input() <- &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   nil,
+		Value: sarama.StringEncoder(m),
 	}
-	var err error = producer.Produce(kafkamsg, nil)
-
-	for err != nil {
-		log.Errorf("ERROR PRODUCING TO KAFKA %v", err)
-		producer.Flush(1)
-		err = producer.Produce(kafkamsg, nil)
-	}
-
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/carbonblack/cb-event-forwarder
 
 require (
 	github.com/RackSec/srslog v0.0.0-20180514150917-1f7cff998e92
+	github.com/Shopify/sarama v1.26.4
 	github.com/aws/aws-sdk-go v1.13.49
 	github.com/carbonblack/ttlru v1.0.10-0.20190816192330-6ef8e99980e4
 	github.com/confluentinc/confluent-kafka-go v0.11.4
@@ -10,7 +11,7 @@ require (
 	github.com/go-ini/ini v1.36.0
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.3.2
-	github.com/google/go-cmp v0.2.0
+	github.com/google/go-cmp v0.4.0
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/gregjones/httpcache v0.0.0-20190212212710-3befbb6ad0cc
 	github.com/hpcloud/tail v1.0.0
@@ -18,18 +19,16 @@ require (
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/klauspost/compress v1.10.4 // indirect
 	github.com/klauspost/pgzip v1.2.3
-	github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563
 	github.com/sirupsen/logrus v1.3.0
 	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
 	github.com/streadway/amqp v0.0.0-20180315184602-8e4aba63da9f
-	github.com/stretchr/testify v1.3.0
 	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec
-	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5 // indirect
-	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	golang.org/x/crypto v0.0.0-20200602180216-279210d13fed // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/oauth2 v0.0.0-20190212230446-3e8b2be13635
-	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
-	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/h2non/filetype.v1 v1.0.5
 	gopkg.in/ini.v1 v1.41.0 // indirect
