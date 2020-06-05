a13e79c5196224e27f2f30be094f5c72675624c2
Zachary Estep <zestep@carbonblack.com>
2020-06-05 08:03:23 -0400

    cb-31401 : Restore function of config options for kafka

--- cmd/cb-event-forwarder/kafka_output.go
+++ cmd/cb-event-forwarder/kafka_output.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"strings"
@@ -18,6 +22,7 @@ import (
 type KafkaOutput struct {
 	brokers           []string
 	topicSuffix       string
+	topic             *string
 	producer          sarama.AsyncProducer
 	droppedEventCount int64
 	eventSentCount    int64
@@ -36,10 +41,74 @@ func (o *KafkaOutput) Initialize(unused string) error {
 
 	o.brokers = strings.Split(*config.KafkaBrokers, ",")
 	o.topicSuffix = config.KafkaTopicSuffix
+	if len(config.KafkaTopic) > 0 {
+		o.topic = &config.KafkaTopic
+	}
 
 	kafkaConfig := sarama.NewConfig()
 	sarama.MaxRequestSize = config.KafkaMaxRequestSize
 	kafkaConfig.Producer.Return.Successes = true
+
+	var useTLS = false
+	var tlsConfig = tls.Config{}
+	if config.KafkaSSLKeyLocation != nil && config.KafkaSSLCertificateLocation != nil {
+		useTLS = true
+		var sslKeyPair, err = tls.LoadX509KeyPair(*config.KafkaSSLKeyLocation, *config.KafkaSSLCertificateLocation)
+		if err != nil {
+			log.Fatalf("Could not load x509 pair for kafka client")
+		}
+		tlsConfig.Certificates = make([]tls.Certificate, 1)
+		tlsConfig.Certificates[0] = sslKeyPair
+	}
+	if config.KafkaSSLCALocation != nil {
+		useTLS = true
+		var certPool, err = x509.SystemCertPool()
+		if err != nil {
+			log.Fatalf("Could not create new cet pool for kafka client")
+		}
+		cf, e := ioutil.ReadFile(*config.KafkaSSLCALocation)
+		if e != nil {
+			log.Fatalf("Could not open ca cert file for kafka client")
+		}
+		cpb, _ := pem.Decode(cf)
+		crt, e := x509.ParseCertificate(cpb.Bytes)
+		if e != nil {
+			log.Fatalf("Could not parse ca cert for kafka client")
+		}
+		certPool.AddCert(crt)
+		tlsConfig.RootCAs = certPool
+	}
+
+	if useTLS {
+		kafkaConfig.Net.TLS.Enable = true
+		kafkaConfig.Net.TLS.Config = &tlsConfig
+	}
+
+	if config.KafkaCompressionType != nil {
+		var compressionTypeString = *config.KafkaCompressionType
+		var compresionCodec = sarama.CompressionNone
+		switch compressionTypeString {
+		case "gzip":
+			compresionCodec = sarama.CompressionGZIP
+		case "zstd":
+			compresionCodec = sarama.CompressionZSTD
+		case "snappy":
+			compresionCodec = sarama.CompressionSnappy
+		case "lz4":
+			compresionCodec = sarama.CompressionLZ4
+		default:
+			compresionCodec = sarama.CompressionNone
+		}
+		if compresionCodec != sarama.CompressionNone {
+			kafkaConfig.Producer.Compression = compresionCodec
+		}
+	}
+
+	if len(config.KafkaUsername) > 0 && len(config.KafkaPassword) > 0 {
+		kafkaConfig.Net.SASL.User = config.KafkaUsername
+		kafkaConfig.Net.SASL.Password = config.KafkaPassword
+		kafkaConfig.Net.SASL.Enable = true
+	}
 
 	producer, err := sarama.NewAsyncProducer(o.brokers, kafkaConfig)
 
@@ -71,17 +140,21 @@ func (o *KafkaOutput) Go(messages <-chan string, errorChan chan<- error) error {
 		for {
 			select {
 			case message := <-messages:
-				var parsedMsg map[string]interface{}
-
-				json.Unmarshal([]byte(message), &parsedMsg)
-				topic := parsedMsg["type"]
-				if topicString, ok := topic.(string); ok {
-					topicString = strings.Replace(topicString, "ingress.event.", "", -1)
-					topicString += o.topicSuffix
-
-					o.output(topicString, message)
+				if o.topic != nil {
+					o.output(*o.topic, message)
 				} else {
-					log.Info("ERROR: Topic was not a string")
+					var parsedMsg map[string]interface{}
+
+					json.Unmarshal([]byte(message), &parsedMsg)
+					topic := parsedMsg["type"]
+					if topicString, ok := topic.(string); ok {
+						topicString = strings.Replace(topicString, "ingress.event.", "", -1)
+						topicString += o.topicSuffix
+
+						o.output(topicString, message)
+					} else {
+						log.Info("ERROR: Topic was not a string")
+					}
 				}
 			}
 		}
