35a530464c0792733c3a65d006509e3f3397b014
Zachary Estep <zestep@carbonblack.com>
2021-09-30 15:40:27 -0400

    CB-37035: Rotate logs inter-day

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -1,12 +1,9 @@
 package logging
 
 import (
-	"fmt"
 	log "github.com/sirupsen/logrus"
-	"os"
+	"github.com/natefinch/lumberjack"
 	"path"
-	"syscall"
-	"time"
 )
 
 const DEFAULTLOGFILENAME = "cb-event-forwarder.log"
@@ -22,43 +19,17 @@ func NewLogFileHandler(location string, level log.Level) LogFileHandler {
 }
 
 func (lh *LogFileHandler) InitializeLogging() {
-	lh.rollOldLogFileIfExists()
 	lh.setUpLogger()
 }
 
-func getTimestampedLogFileName(logTime time.Time) string {
-	return fmt.Sprintf("%s-%s.log", "cb-event-forwarder", logTime.UTC().Format(time.RFC3339))
-}
-
-func (lh *LogFileHandler) getRolledLogFileNameWithPath(logTime time.Time) string {
-	return path.Join(lh.location, getTimestampedLogFileName(logTime))
-}
-
-func (lh *LogFileHandler) rollOverLogFile(timeStamp time.Time) {
-	err := os.Rename(lh.fileNameWithPath, lh.getRolledLogFileNameWithPath(timeStamp))
-	if err != nil {
-		log.Panicf("Couldn't roll log file: %v", err)
-	}
-}
-
-func (lh *LogFileHandler) rollOldLogFileIfExists() {
-	if fileInfo, err := os.Stat(lh.fileNameWithPath); err == nil {
-		stat := fileInfo.Sys().(*syscall.Stat_t)
-		createTime := time.Unix(stat.Ctim.Sec, stat.Ctim.Nsec)
-		lh.rollOverLogFile(createTime)
-	}
-}
-
-func (lh *LogFileHandler) openForwarderLogFile() (*os.File, error) {
-	file, err := os.OpenFile(lh.fileNameWithPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	return file, err
-}
-
 func (lh *LogFileHandler) setUpLogger() {
-	outputFile, err := lh.openForwarderLogFile()
-	if err != nil {
-		log.Panicf("Unable to initialize logger %e", err)
-	}
-	log.SetOutput(outputFile)
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   lh.fileNameWithPath,
+		MaxSize:    50,
+		MaxBackups: 7,
+		MaxAge:     1,
+		Compress:   true,
+		LocalTime: true,
+	})
 	log.SetLevel(lh.level)
 }
