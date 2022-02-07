9b6b49de465a2567d0a3e30054e96181dc513f73
Zachary Estep <zestep@carbonblack.com>
2022-02-07 14:05:07 -0500

    CB-37038: Add container build

--- run-event-forwarder.sh
+++ run-event-forwarder.sh
@@ -6,17 +6,31 @@ CONFIG_DIR=/etc/cb/integrations/event-forwarder
 LOG_DIR_EXTERNAL=/var/log/cb/integrations/cb-event-forwarder
 LOG_DIR=/var/log/cb/integrations/cb-event-forwarder
 MOUNT_POINTS="--mount type=bind,source=$CONFIG_DIR_EXTERNAL,target=$CONFIG_DIR --mount type=bind,source=$LOG_DIR_EXTERNAL,target=$LOG_DIR"
-SERVICE_START='/usr/share/cb/integrations/event-forwarder/cb-event-forwarder /etc/cb/integrations/event-forwarder/cb-event-forwarder.conf'
-CONTAINER_RUNNING=$(docker inspect --format="{{.State.Running}}" $LABEL 2> /dev/null)
-if [ "$CONTAINER_RUNNING" == "true" ]; then
-  STARTUP_COMMAND="echo EDR Event Forwarder container already running"
-  STATUS_COMMAND="echo EDR Event Forwader is running"
-else
-  STARTUP_COMMAND="docker run -d --rm $MOUNT_POINTS --name $LABEL $IMAGE $SERVICE_START"
-  STATUS_COMMAND="echo EDR Event Forwarder container is stopped"
-fi
+SERVICE_START=/usr/share/cb/integrations/event-forwarder/cb-event-forwarder 
+
+get_container_status () {
+    CONTAINER_NAME=$(docker ps | grep $LABEL | head -n1 | awk '{print $1}')
+    if [ "${#CONTAINER_NAME}" -gt 0 ]; then
+        CONTAINER_RUNNING=true
+        echo "EDR Event Forwarder Container status: Running"
+        echo "EDR Event Forwarder Container identifier: ${CONTAINER_NAME}"
+    else
+        # run ps with -a switch to see if stopped or non-existent
+        STOPPED_NAME=$(docker ps | grep $LABEL | head -n1 | awk '{print $1}')
+        if [ "${#STOPPED_NAME}" -gt 0 ]; then
+            echo "EDR Event Forwarder Container status: Stopped "
+        else
+            echo "EDR Event Forwarder Container status: No running container"
+        fi
+        CONTAINER_RUNNING=false
+    fi
+}
+
+STATUS_COMMAND=get_container_status
+
 SHUTDOWN_COMMAND="docker stop $LABEL"
 
+STARTUP_COMMAND="docker run -d --rm $MOUNT_POINTS --name $LABEL $IMAGE $SERVICE_START"
 
 print_help() {
   echo "Usage: edr-eventforwarder-run COMMAND [options]"
@@ -42,7 +56,7 @@ if [[ "${1}" == "" ]]; then
 fi
 
 if [[ "${1^^}" =~ ^(START|STOP|STATUS)$ ]]; then
-  echo "EDR Event Forwarder: running ${1}..."
+  echo "EDR Event Forwarder: running command ${1}..."
   case "${1^^}" in
     START) $STARTUP_COMMAND ;;
     STOP) $SHUTDOWN_COMMAND ;;
